289bcecba1ac072ec819853fc2667474bc54331f
Clément Bartholomé <clementoss@gmail.com>
2025-08-03 16:49:38 +0200

    feat(user): méthode pour récupérer le current user id

---
+++ MonEndoVue.Server/Services/UserExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Security.Claims;
+namespace MonEndoVue.Server.Services;
+
+public static class UserExtensions
+{
+    public static string GetCurrentUserId(this ClaimsPrincipal user)
+    {
+        return user.FindFirstValue(ClaimTypes.NameIdentifier) ?? string.Empty;
+    }
+}
